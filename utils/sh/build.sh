1b6e2cc1d579f5ca099c1b9307ba9cdac5e77f8d
Spencer Elkington <spelkington@gmail.com>
2023-04-21 18:10:52 -0600

    build structure changes

--- utils/sh/build.sh
+++ utils/sh/build.sh
@@ -7,11 +7,11 @@ mkdir builds
 
 for project_file in ./*.project.json; do
     # Capture domain (domain.project.json) from filename
-    project_domain=$( echo $project_file | sed 's/^\.\/\([^.]*\).*/\1/' )
+    project_name=$(jq -r ".name" $project_file)
 
     # Create build directories with rblx file + project file
-    echo "Building $project_domain.rblx into /$project_domain..."
-    mkdir builds/$project_domain
-    rojo build --output builds/$project_domain/$project_domain.rbxl $project_file
-    cp $project_file builds/$project_domain/
+    echo "Building $project_name.rblx into builds/$project_name..."
+    mkdir builds/$project_name
+    rojo build --output builds/$project_name/$project_name.rbxl $project_file >> /dev/null
+    cp $project_file builds/$project_name/
 done
