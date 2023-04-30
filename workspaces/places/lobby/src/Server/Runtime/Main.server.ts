610c072b08105f60f9e8832623e0bd1844161b3d
Spencer Elkington <spelkington@gmail.com>
2023-04-29 19:40:41 -0600

    Fixed Knit transition (#25)
    * building
    
    * template deployed
    
    * Added basemodels to pull
    
    * Asset compilation process refined
    
    * Moved to Knit
    
    * Dependency updates
    
    * Add lobby

--- workspaces/places/lobby/src/Server/Runtime/Main.server.ts
+++ workspaces/places/lobby/src/Server/Runtime/Main.server.ts
@@ -1,9 +1,15 @@
 import { KnitServer as Knit, Component } from "@rbxts/knit";
-import { ServerScriptService } from "@rbxts/services";
+import { Workspace, ServerStorage, ServerScriptService, Players } from "@rbxts/services";
+
 import { makeHello } from "Shared/HelloWorldModule";
 
 Knit.AddServices(ServerScriptService.Services);
 Knit.Start();
 Component.Auto(ServerScriptService.Components);
+
+// Spawn the lobby.
+const lobbyModel = ServerStorage.Assets.Maps.Lobbies.FindFirstChildOfClass("Model");
+const newLobby = lobbyModel!.Clone();
+newLobby.Parent = Workspace;
 
 print(makeHello("main.server.ts"));
